c737258d9fb0ae337668019f789cde8236290ef5
siyahsensei <muratkara.dev@gmail.com>
2025-12-07 17:39:24 +0300

    tech: dockerfile added for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+    output: "standalone",
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer-when-downgrade',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self' http: https: data: blob: 'unsafe-inline'",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
